64ff0c48d14d4740a6db49b5ba19816e0a0d64fe
kboyko <kboyko1999@gmail.com>
2020-03-03 12:12:11 +0200

    chore: try to use vue configuration

--- docker/3-inside-container-build-everything.sh
+++ docker/3-inside-container-build-everything.sh
@@ -1,10 +1,10 @@
 time su -c "cd /project/site && npm run build" fo
 
-mv /var/www/html /var/www/html-oldnode
-ln -s /project/site/build /var/www/html
-
-fo-fix-dir-ownership /etc/nginx/conf.d/
-
+#mv /var/www/html /var/www/html-oldnode
+#ln -s /project/site/build /var/www/html
+#
+#fo-fix-dir-ownership /etc/nginx/conf.d/
+#
 do-default
 
-touch /should-not-chown-finally
+#touch /should-not-chown-finally
--- docker/5-inside-container-start-working.sh
+++ docker/5-inside-container-start-working.sh
@@ -1,7 +1,7 @@
 nginx -g 'daemon off;' &
 
-cd /project/site
-
-npm run start 2>&1 | tee -a /storage/logs.txt
+#cd /project/site
+#
+#npm run start 2>&1 | tee -a /storage/logs.txt
 
 #do-default
