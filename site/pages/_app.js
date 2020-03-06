305f17b3d5b1a307b0748a55a71fe2e31ca4dbce
kboyko <kboyko1999@gmail.com>
2020-03-06 16:49:06 +0200

    feat: add og markup for main pages

--- site/pages/_app.js
+++ site/pages/_app.js
@@ -14,6 +14,12 @@ const MyApp = ({ Component, pageProps }) => (
       <title>Ш++ збір коштів</title>
       <link rel="shortcut icon" href="/favicon.ico" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta property="og:title" content="Ш++ - збір коштів" />
+      <meta property="og:url" content={process.env.APP_URL} />
+      <meta property="fb:app_id" content="1566470086989294" />
+      <meta property="og:image" content="/cover-image.png" />
+      <meta property="og:site_name" content="Підтримай++ - спільнокошт" />
+      <meta property="og:description" content="Підтримай Ш++ - незалежний соціально-культурний проект у Кропивницькому." />
     </Head>
     {/* eslint-disable-next-line react/jsx-props-no-spreading */}
     <Component {...pageProps} />
