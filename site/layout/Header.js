54239093e1c7e8183d7842496f205a8e6c93d8dc
kboyko <kboyko1999@gmail.com>
2020-04-12 17:38:02 +0300

    chore: support translation for Header

--- site/layout/Header.js
+++ site/layout/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import Link from 'next/link';
+// import Link from 'next/link';
+import { i18n, withTranslation, Link } from '../utils/translations';
 import colors from '../theme/colors';
 
 const styles = {
@@ -39,26 +40,44 @@ const styles = {
   },
 };
 
-const Header = ({ links }) => (
-  <Navbar collapseOnSelect expand="md" style={styles.container}>
-    <Link href="/">
-      <Navbar.Brand>
-        <span style={styles.title} className="title">
-          <span style={styles.crowdfunding} className="logo">Підтримай<span className="text-green">++</span></span>
-        </span>
-      </Navbar.Brand>
-    </Link>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav" style={{ flexGrow: 0, width: '100%', justifyContent: 'flex-end' }}>
-      {
-        links.map((l) => (
-          <Link href={l.href} key={l.href}>
-          </Link>
-        ))
-      }
-    </Navbar.Collapse>
-  </Navbar>
-);
+const Header = ({ links, t }) => {
+  return (
+    <Navbar collapseOnSelect expand="md" style={styles.container}>
+      <Link href="/">
+        <Navbar.Brand>
+          <span style={styles.title} className="title">
+            <span style={styles.crowdfunding} className="logo">{t('logo')}<span className="text-green">++</span></span>
+          </span>
+        </Navbar.Brand>
+      </Link>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav" style={{ flexGrow: 0, width: '100%', justifyContent: 'flex-end' }}>
+        {
+          links.map((l) => (
+            <Link href={l.href} key={l.href}>
               <a className="list-item">{l.text}</a>
+            </Link>
+          ))
+        }
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <button
+            type="button"
+            className="translate-button"
+            onClick={() => i18n.changeLanguage('en')}
+          >
+            EN
+          </button>
+          <button
+            type="button"
+            className="translate-button"
+            onClick={() => i18n.changeLanguage('uk')}
+          >
+            UA
+          </button>
+        </div>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-export default Header;
+export default withTranslation('header')(Header);
