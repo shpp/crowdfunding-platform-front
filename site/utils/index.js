9d8a0002decc51c7ff3feba6add45850295e9ec5
kboyko <kboyko1999@gmail.com>
2020-04-12 17:40:05 +0300

    chore: add some translation to utils

--- site/utils/index.js
+++ site/utils/index.js
@@ -1,14 +1,17 @@
-export function formatDate(date) {
-  const months = ['січня', 'лютого', 'березня', 'квітня', 'травня', 'червня', 'липня', 'серпня', 'вересня', 'жовтня', 'листопада', 'грудня'];
+export function formatDate(date, lang) {
+  const months = {
+    uk: ['січня', 'лютого', 'березня', 'квітня', 'травня', 'червня', 'липня', 'серпня', 'вересня', 'жовтня', 'листопада', 'грудня'],
+    en: ['of Jan.', 'of Feb.', 'of Mar.', 'of Apr.', 'of May.', 'of Jun.', 'of Jul.', 'of Aug.', 'of Sep.', 'of Oct.', 'of Nov.', 'of Dec.'],
+  };
   // eslint-disable-next-line no-param-reassign
   date = new Date(date);
   const day = date.getDate();
   const month = date.getMonth();
   const year = date.getFullYear();
-  const yearStr = new Date().getFullYear() === year ? '' : `${year}р.`;
-  return `${day} ${months[month]} ${yearStr}`;
+  const yearStr = new Date().getFullYear() === year ? '' : `${year}${lang === 'uk' ? 'р.' : ''}`;
+  return `${day} ${months[lang][month]} ${yearStr}`;
 }
-export const formatMoney = (x) => `${new Intl.NumberFormat('ru-RU').format(x)} грн`;
+export const formatMoney = (x, lang) => `${new Intl.NumberFormat(lang).format(x)} ${lang === 'uk' ? 'грн' : 'UAH'}`;
 export const isLastThreeMonths = (date) => {
   const today = new Date();
   const startOfCurrentMonth = +new Date(today.getFullYear(), today.getMonth(), 1);
