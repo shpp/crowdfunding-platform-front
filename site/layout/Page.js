ea15846babf4a21524392b51386390a10817b160
kboyko <kboyko1999@gmail.com>
2020-04-12 17:39:05 +0300

    chore: support translation for main Layout

--- site/layout/Page.js
+++ site/layout/Page.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import Header from './Header';
 import Footer from './Footer';
+import { withTranslation } from '../utils/translations';
 
-const Layout = ({ children }) => (
+const Layout = ({ children, t }) => (
   <div className="wrapper">
     <Header links={[
-      { href: '/help', text: 'Про нас' },
-      { href: '/', text: 'Проекти' },
-      { href: { pathname: '/', query: { filter: 'completed' } }, text: 'Вже зібрали' },
-      { href: '#contacts', text: 'Контакти' },
+      { href: '/about', text: t('about') },
+      { href: '/', text: t('projects') },
+      { href: { pathname: '/', query: { filter: 'completed' } }, text: t('completed') },
+      { href: '#contacts', text: t('contacts') },
     ]}
     />
     <main>{children}</main>
@@ -18,4 +19,8 @@ const Layout = ({ children }) => (
   </div>
 );
 
-export default Layout;
+Layout.getInitialProps = async () => ({
+  namespacesRequired: ['header', 'help', 'footer'],
+});
+
+export default withTranslation('header')(Layout);
