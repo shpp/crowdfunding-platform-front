a4cb8cc21088315ef3f563d04b0677ac7aaa9c76
Kate <kboyko1999@gmail.com>
2021-02-11 00:26:03 +0200

    chore: disable server language detection

--- site/utils/translations.js
+++ site/utils/translations.js
@@ -3,6 +3,7 @@ const NextI18Next = require('next-i18next').default;
 module.exports = new NextI18Next({
   defaultLanguage: 'uk',
   otherLanguages: ['en'],
+  serverLanguageDetection: false,
   localeSubpaths: {
     en: 'en'
   }
