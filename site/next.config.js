83c62e29e2460228ad6096216367100409bdde1f
kos_ua <kost.yakimenko@gmail.com>
2024-05-30 10:51:23 +0300

    chore: added public environments to next.config

--- site/next.config.js
+++ site/next.config.js
@@ -8,6 +8,15 @@ module.exports = {
       return `${new Date().getTime()}`;
     }
   },
+  env: {
+    API_URL: process.env.NEXT_PUBLIC_API_URL,
+    APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    SHEETS_URL: process.env.NEXT_PUBLIC_SHEETS_URL,
+    HASH_SALT: process.env.NEXT_PUBLIC_HASH_SALT,
+    SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    ALTERNATIVE_DONATE_LINK: process.env.NEXT_PUBLIC_ALTERNATIVE_DONATE_LINK,
+    GOOGLE_ANALYTICS_KEY: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY
+  },
   serverRuntimeConfig: {},
   publicRuntimeConfig: {
     DEPLOY_ENV: process.env.DEPLOY_ENV,
